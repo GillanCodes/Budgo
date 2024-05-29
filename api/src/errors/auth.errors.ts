93454543ba139d721f7267c36a2a6f682055e2bf
gillancodes <gillancodes@laposte.net>
2024-05-29 10:32:04 +0200

    feat: added login errors

---
+++ api/src/errors/auth.errors.ts
@@ -0,0 +1,15 @@
+export const loginErrors = (error:any) => {
+    let errors = {logs: "", password: ""};
+
+    if (error.message.includes("_id"))
+    {
+        errors.logs = "Incorrect 'Username' or 'Email'";
+    }
+
+    if (error.message.includes("Incorrect logs"))
+    {
+        errors.password = "Incorrect logs or password"
+    }
+
+    return errors;
+};
