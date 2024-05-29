cb4396417aec0b0766efa46eb1ddac02e8fbaffb
gillancodes <gillancodes@laposte.net>
2024-05-29 10:10:01 +0200

    feat: added checkUser middleware

---
+++ api/middlewares/auth.middleware.ts
@@ -0,0 +1,25 @@
+import config from "../config/config";
+import * as jwt from 'jsonwebtoken';
+import userModel from "../models/user.model";
+import * as express from 'express';
+
+module.exports.checkUser = async(req: express.Request, res: express.Response, next: () => void) => {
+    let token = req.cookies.auth;
+    if (token)
+    {
+        jwt.verify(token, config.JWT_TOKEN, async(err: any, decodedToken: any) => {
+            if (err)
+            {
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await userModel.findById(decodedToken.id).select('-password');
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
