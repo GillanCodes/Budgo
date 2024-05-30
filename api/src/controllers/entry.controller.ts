37b31521e741bec321701b7e8428180e26132aa2
gillancodes <gillancodes@laposte.net>
2024-05-30 12:27:20 +0200

    feat: postEntry done

--- api/src/controllers/entry.controller.ts
+++ api/src/controllers/entry.controller.ts
@@ -1,4 +1,6 @@
 import * as express from "express"
+import { isEmpty } from "validator";
+import entryModel from "../../models/entry.model";
 
 export const getEntries = (req: express.Request, res: express.Response) => {
 
@@ -8,8 +10,30 @@ export const getEntry = (req: express.Request, res: express.Response) => {
 
 }
 
-export const postEntries = (req: express.Request, res: express.Response) => {
+export const postEntry = (req: express.Request, res: express.Response) => {
+    try {
+        
+        const {date, type, amount} = req.body;
 
+        if (isEmpty(date) || isEmpty(type) || isEmpty(amount) )
+            throw Error("post_entry_empty_field");
+
+        entryModel.create({
+            userId: res.locals.user._id,
+            date,
+            type,
+            amount
+        }).then((data) => {
+            return res.status(201).send(data);
+        }).catch((err) => {
+            //TODO: Error Handle
+            console.log(err);
+        })
+
+    } catch (error) {
+        //TODO : Error Handle
+        console.log("error")
+    }
 }
 
 export const patchEntry = (req: express.Request, res: express.Response) => {
--- api/src/routes/entry.routes.ts
+++ api/src/routes/entry.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { deleteEntries, getEntries, getEntry, patchEntry, postEntries } from '../controllers/entry.controller';
+import { deleteEntries, getEntries, getEntry, patchEntry, postEntry } from '../controllers/entry.controller';
 let router:Router = Router();
 
 router.get('/', getEntries);
 router.get('/:id', getEntry);
 
-router.post('/', postEntries);
+router.post('/', postEntry);
 
 router.patch('/:id', patchEntry);
 
