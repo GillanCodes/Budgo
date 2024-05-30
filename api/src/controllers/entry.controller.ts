e1f2cad272abe4946c9b5006cf0ce90be129a9e7
gillancodes <gillancodes@laposte.net>
2024-05-30 11:59:27 +0200

    feat: base entries controller done

---
+++ api/src/controllers/entry.controller.ts
@@ -0,0 +1,21 @@
+import * as express from "express"
+
+export const getEntries = (req: express.Request, res: express.Response) => {
+
+}
+
+export const getEntry = (req: express.Request, res: express.Response) => {
+
+}
+
+export const postEntries = (req: express.Request, res: express.Response) => {
+
+}
+
+export const patchEntry = (req: express.Request, res: express.Response) => {
+
+}
+
+export const deleteEntries = (req: express.Request, res: express.Response) => {
+
+}
