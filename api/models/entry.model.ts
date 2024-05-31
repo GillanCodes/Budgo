c9596fe57735d2380903b299c6ec827c687cc623
gillancodes <gillancodes@laposte.net>
2024-05-31 09:36:13 +0200

    feat: added entry model

--- api/models/entry.model.ts
+++ api/models/entry.model.ts
@@ -4,6 +4,7 @@ export interface IEntry extends Document
 {
     _id: ObjectId | string,
     userId: ObjectId | string,
+    name: string,
     date: Date | number | string,
     type: string | number,
     amount: number | string,
@@ -13,6 +14,7 @@ export interface IEntry extends Document
 
 const entrySchema = new Schema<IEntry>({
     userId: {type: String, required: true},
+    name: {type: String, default:"Default Entry"},
     date: {type: String, required: true},
     type: {type: String, default: "expense"},
     amount: {type: Number, required:true},
